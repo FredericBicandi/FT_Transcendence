1e54aa97a6fc0ddff3c85091d3fbee0af12bc77a
fbicandy <freddy@instance-20260505-201049.c.active-dahlia-495419-i0.internal>
2026-05-16 17:09:27 +0000

    latest server

--- obj/Debug/net8.0/FT_Transcendence.AssemblyInfo.cs
+++ obj/Debug/net8.0/FT_Transcendence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FT_Transcendence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33bd939866801219775c4e986eb797c63409138f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df083d02d5b100af063e2e9af0825cf64c32a7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FT_Transcendence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FT_Transcendence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
